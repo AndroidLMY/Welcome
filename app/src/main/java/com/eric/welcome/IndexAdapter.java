0b47eea09690001afb00384c704e6c68e7a3fb97
mr.li <465008238@qq.com>
2019-11-06 15:44:17 +0800

    解决编译错误

--- app/src/main/java/com/eric/welcome/IndexAdapter.java
+++ app/src/main/java/com/eric/welcome/IndexAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
